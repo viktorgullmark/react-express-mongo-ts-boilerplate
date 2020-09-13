8f739d4f6a0ecbb74d753e7ffb59b35360d3733a
Viktor Gullmark <viktor.gullmark@gmail.com>
2020-09-13 18:29:13 +0200

    Added bodyparser

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,12 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 import { Request, Response } from 'express';
 
 const app = express();
 const PORT = 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req: Request, res: Response) => {
   res.send({
